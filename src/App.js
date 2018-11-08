1b322665a0e6b99e884729919381b70eb7eb7dd3
SokalMet <sokalmet@ukr.net>
2018-11-08 11:57:42 +0200

    refactored

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import {Route} from 'react-router-dom'
 import SearchBook from "./SearchBook"
 import HomePage from "./Home"
 
-class BooksApp extends React.Component {
-  render() {
-    return (
-      <div className="app" data-test="component-app">
-        <Route path='/search' component={SearchBook} />
-        <Route exact path='/' component={HomePage} />
-      </div>
-    )
-  }
+const BooksApp = () => {
+  return (
+    <div className="app" data-test="component-app">
+      <Route path='/search' component={SearchBook}/>
+      <Route exact path='/' component={HomePage}/>
+    </div>
+  )
 }
 
 export default BooksApp
