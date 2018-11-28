89f30eadec2b796c3986dac80082e0213f231152
SokalMet <sokalmet@ukr.net>
2018-11-28 12:23:59 +0200

    test to have two routes in App

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Route} from 'react-router-dom'
+import { LocalStorage } from 'jest-localstorage-mock';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App'
@@ -7,7 +9,12 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 it('renders without crashing', () => {
   const wrapper = shallow(<App />)
-  expect(wrapper.find('[data-test="component-app"]').length).toBe(1)
+  expect(wrapper.find('[data-test="component-app"]').length).tobe(1)
+})
+
+it('has two routes', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find(Route).length).toBe(2)
 })
 
 
