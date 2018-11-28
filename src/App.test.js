dab677d2f2e86b0f59b4fdb6a4a7631423b93866
SokalMet <sokalmet@ukr.net>
2018-11-28 12:52:34 +0200

    refactored a bit

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
-import { LocalStorage } from 'jest-localstorage-mock';
-import Enzyme, { shallow } from 'enzyme';
+import {LocalStorage} from 'jest-localstorage-mock';
+import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App'
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+Enzyme.configure({adapter: new EnzymeAdapter()});
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper.find('[data-test="component-app"]').length).toBe(1)
+describe('<App />', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+  })
+
+  it('renders without crashing', () => {
+    expect(wrapper.find('[data-test="component-app"]').length).toBe(1)
+  })
+
+  it('has two routes', () => {
+    expect(wrapper.find(Route).length).toBe(2)
+  })
 })
-
-it('has two routes', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper.find(Route).length).toBe(2)
-})
-
-
